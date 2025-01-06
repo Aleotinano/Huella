56eb94c7f80cdfc3011b8339dd3a99505c6075da
Rodrigo <ranaraniada2@gmail.com>
2025-01-06 19:34:56 -0300

    Logout

--- Backend/authentication/urls.py
+++ Backend/authentication/urls.py
@@ -9,6 +9,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('Register/', views.UserPostView.as_view(), name='register'),
     path('UserPanel/', views.UserDetailView.as_view(), name='user_detail'),
+    path('Logout/', views.LogoutView.as_view(), name='logout'),
     path('Login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('Login/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
