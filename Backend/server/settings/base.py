d791d7f65404b20809fff238a3ecb4f5911e9cd4
Rodrigo <ranaraniada2@gmail.com>
2025-01-06 20:12:17 -0300

    Proteccion de rutas 'products/'

--- Backend/server/settings/base.py
+++ Backend/server/settings/base.py
@@ -167,8 +167,8 @@ CORS_ALLOW_CREDENTIALS = True  # Permite credenciales
 
 #TOKEN
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=20),
-    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
 }
